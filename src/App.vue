84b88a1e80ca2cd4c1ffe83686557a0166e1f8de
DrKillex <lcollaro1994@gmail.com>
2023-03-31 18:06:32 +0200

    funzione unica per ricerca

--- src/App.vue
+++ src/App.vue
@@ -11,41 +11,61 @@ export default {
   },
   data() {
     return {
-      store
+      store,
+      typeSearch: ""
     }
   },
   methods: {
-    searchMovie(){
-      axios.get('https://api.themoviedb.org/3/search/movie?',
+    searchMethod(url, type){
+      axios.get( url + type ,
       {params: {
-        api_key: store.config.api_key,
-        language: store.config.lang,
-        query: store.search
+        api_key: this.store.config.api_key,
+        language: this.store.config.lang,
+        query: this.store.search       
+      }})
+      .then((response) => {
+        console.log(type);
+        console.log(response.data.results);
+        if (type === '/movie') {
+          this.store.moviesResults = response.data.results
+        } else {
+          this.store.seriesResults = response.data.results
+        }
+      })      
+    },
+    // searchMovie(){
+    //   axios.get('https://api.themoviedb.org/3/search/movie',
+    //   {params: {
+    //     api_key: store.config.api_key,
+    //     language: store.config.lang,
+    //     query: store.search
         
-      }})
-      .then((response) => {
-        console.log(response.data.results);
-        this.store.moviesResults= response.data.results
-      })      
-    },
-    searchSeries(){
-      axios.get('https://api.themoviedb.org/3/search/tv?',
-      {params: {
-        api_key: store.config.api_key,
-        query: store.search
-      }})
-      .then((response) => {
-        console.log(response.data.results);
-        this.store.seriesResults= response.data.results.map(element => {
-          element.original_title = element.original_name
-          element.title = element.name
-          return element
-        })
-      })      
-    },
+    //   }})
+    //   .then((response) => {
+    //     console.log(response.data.results);
+    //     this.store.moviesResults= response.data.results
+    //   })      
+    // },
+    // searchSeries(){
+    //   axios.get('https://api.themoviedb.org/3/search/tv',
+    //   {params: {
+    //     api_key: store.config.api_key,
+    //     query: store.search
+    //   }})
+    //   .then((response) => {
+    //     console.log(response.data.results);
+    //     this.store.seriesResults= response.data.results.map(element => {
+    //       element.original_title = element.original_name
+    //       element.title = element.name
+    //       return element
+    //     })
+    //   })      
+    // },
     search(){
-      this.searchMovie(),
-      this.searchSeries()
+      this.searchMethod(this.store.config.urlSearch, this.store.config.searchMovie)
+      this.searchMethod(this.store.config.urlSearch, this.store.config.searchSeries)
+      // this.searchMovie(),
+      // this.searchSeries()
     }
   },
 }
--- src/store.js
+++ src/store.js
@@ -2,9 +2,11 @@ import { reactive } from "vue";
 
 export const store = reactive({
     config:{
-        // searchMovie: "/search/movie",
+        urlSearch: "https://api.themoviedb.org/3/search",
+        searchMovie: "/movie",
+        searchSeries: "/tv",
         api_key: "c3ef55e2302baf7fcda82cfe154ff68a",
-        lang: "it-IT"
+        lang: "it-IT",
     },
     search: "",
     moviesResults: [],
